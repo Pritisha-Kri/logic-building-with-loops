2b34b49222b6588697cc52572b1630c01e1bd57e
Pritisha Kumari <your.pritishakumari.official@gmail.com>
2026-03-11 00:34:50 +0530

    update

--- 1.while-loop/divisibleby7.java
+++ 1.while-loop/divisibleby7.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class divisibleby7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a and b");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int i=a;
+
+        while(i<=b){
+            if(i%7 ==0){
+                System.out.println(i);
+            }
+            i++;
+
+        }
+
+        sc.close();
+
+    }
     
 }
--- 1.while-loop/hcf.java
+++ 1.while-loop/hcf.java
@@ -1,3 +1,20 @@
-public class hcf {
-    
+import java.util.Scanner;
+
+public class hcf{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("HCF = " + a);
+    }
 }
